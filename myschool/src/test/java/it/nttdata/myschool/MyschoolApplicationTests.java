c69161d86a3d2415d9572537ca819b9e51f49d32
michelebellizzi <bellizzi23@gmail.com>
2022-11-28 15:21:22 +0100

    inizio

---
+++ myschool/src/test/java/it/nttdata/myschool/MyschoolApplicationTests.java
@@ -0,0 +1,13 @@
+package it.nttdata.myschool;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyschoolApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
